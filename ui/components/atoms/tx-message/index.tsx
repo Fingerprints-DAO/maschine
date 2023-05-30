ea6fe805c5475e1ec9841477792c8ea5bb5a66a9
Thiago Lobão <thiago@blank.st>
2023-05-30 19:23:25 -0300

    [Toast] Add layout

--- ui/components/atoms/tx-message/index.tsx
+++ ui/components/atoms/tx-message/index.tsx
@@ -4,10 +4,21 @@ type TxMessageProps = {
   hash?: string
 }
 export const TxMessage = ({ hash }: TxMessageProps) => {
-  if (!hash) return <></>
+  if (!hash) {
+    return null
+  }
+
   return (
-    <Box as="a" href={`${process.env.NEXT_PUBLIC_ETHERSCAN_URL}/tx/${hash}`} target="_blank" textDecoration="underline">
-      Click here to see transaction
+    <Box
+      as="a"
+      href={`${process.env.NEXT_PUBLIC_ETHERSCAN_URL}/tx/${hash}`}
+      target="_blank"
+      color="gray.900"
+      fontSize="lg"
+      fontWeight="bold"
+      textDecoration="underline"
+    >
+      view in Etherscan.
     </Box>
   )
 }
--- ui/hooks/use-tx-toast.tsx
+++ ui/hooks/use-tx-toast.tsx
@@ -1,15 +1,59 @@
-import { UseToastOptions, useToast } from '@chakra-ui/react'
+import { CheckIcon, WarningIcon } from '@chakra-ui/icons'
+import { As, CloseButton, Flex, Icon, Text, ToastId, UseToastOptions, useToast } from '@chakra-ui/react'
 import { TxMessage } from '@ui/components/atoms/tx-message'
+
+type ToastContentProps = {
+  title: string
+  status: 'success' | 'error'
+  txHash?: string
+  toastId: ToastId
+  icon?: {
+    as: As
+    color: any
+  }
+  description?: string
+  onClose: (toastId: ToastId) => void
+}
+
+const ToastContent = ({ title, description, txHash, status, toastId, icon, onClose }: ToastContentProps) => {
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      bg={status === 'success' ? 'gray.300' : '#F76B8B'}
+      p={['84px 16px 72px', 4]}
+      position="relative"
+    >
+      {Boolean(icon) && <Icon {...icon} mr={2} />}
+      <Text color="gray.900" fontSize="lg" fontWeight="bold" textAlign="center">
+        {title}
+        {description || (
+          <>
+            {Boolean(txHash) && ', click to '}
+            {Boolean(txHash) && <TxMessage hash={txHash} />}
+          </>
+        )}
+      </Text>
+      <CloseButton color="gray.900" position="absolute" right="7px" top="7px" w="44px" h="44px" size="lg" onClick={() => onClose(toastId)} />
+    </Flex>
+  )
+}
 
 export const useTxToast = () => {
   const toast = useToast()
 
-  const showTxSentToast = (txHash?: string) => {
+  const showTxSentToast = (toastId: ToastId, txHash?: string) => {
     toast({
-      title: 'Transaction sent',
-      description: <TxMessage hash={txHash} />,
-      status: 'success',
+      id: toastId,
       isClosable: true,
+      containerStyle: {
+        width: '100%',
+        maxW: 'unset',
+        m: 0,
+        mt: 1,
+      },
+      duration: 9000,
+      render: () => <ToastContent title="Transaction sent" txHash={txHash} status="success" toastId={toastId} onClose={toast.close} />,
     })
   }
 
@@ -21,13 +65,27 @@ export const useTxToast = () => {
     if (toastOptions.id && toast.isActive(toastOptions.id)) {
       return
     }
+
     toast({
       ...toastOptions,
-      title,
-      description: <TxMessage hash={txHash} />,
-      status: 'success',
       isClosable: true,
+      containerStyle: {
+        width: '100%',
+        maxW: 'unset',
+        m: 0,
+        mt: 1,
+      },
       duration: 9000,
+      render: () => (
+        <ToastContent
+          title={title}
+          txHash={txHash}
+          status="success"
+          icon={{ as: CheckIcon, color: 'gray.900' }}
+          toastId={toastOptions.id!}
+          onClose={toast.close}
+        />
+      ),
     })
   }
 
@@ -36,24 +94,52 @@ export const useTxToast = () => {
 
     if (revertError.errorName) {
       const id = 'error-name'
-      if (toast.isActive(id)) return
+
+      if (toast.isActive(id)) {
+        return
+      }
+
       toast({ title: `An error occured`, description: `Error reverted ${revertError.errorName}`, status: 'error', id })
+
       return
     }
+
     if (revertError.code === 'ACTION_REJECTED') {
       const id = 'user-rejected'
-      if (toast.isActive(id)) return
+
+      if (toast.isActive(id)) {
+        return
+      }
+
       toast({ title: `An error occured`, description: `User rejected metamask tx`, status: 'error', id })
+
       return
     }
 
     const id = 'error'
-    if (toast.isActive(id)) return
+
+    if (toast.isActive(id)) {
+      return
+    }
+
     toast({
       id,
-      title: `An error occured`,
-      description: `Error message: ${revertError.reason ?? revertError.cause ?? revertError.message}`,
-      status: 'error',
+      containerStyle: {
+        width: '100%',
+        maxW: 'unset',
+        m: 0,
+        mt: 1,
+      },
+      render: () => (
+        <ToastContent
+          title="An error occured: "
+          description={revertError.reason ?? revertError.cause ?? revertError.message}
+          status="error"
+          icon={{ as: WarningIcon, color: 'gray.900' }}
+          toastId={id}
+          onClose={toast.close}
+        />
+      ),
     })
   }
 
