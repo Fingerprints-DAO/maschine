d1b785673bac4b29a08e2979c33b1c4cb652c00a
Thiago Lobão <thiago@blank.st>
2023-05-31 00:37:23 -0300

    Move intervals to enum

--- web3/contracts/maschine/use-total-supply.ts
+++ web3/contracts/maschine/use-total-supply.ts
@@ -1,7 +1,8 @@
 import { useQueries } from 'react-query'
 import useMaschine from './use-maschine'
+import { Interval } from 'types/interval'
 
-const refetchInterval = 10000
+const refetchInterval = Interval.TotalSupply
 
 const useTotalSupply = () => {
   const maschineContract = useMaschine()
