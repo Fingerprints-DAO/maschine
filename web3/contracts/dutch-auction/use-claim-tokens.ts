e3bca66f8f2407702f82a6161946a40c3a932f33
arodundef <arod.mail+fp@proton.me>
2023-06-07 13:36:18 -0300

    fix(gas): improve gas calculation

--- web3/contracts/dutch-auction/use-claim-tokens.ts
+++ web3/contracts/dutch-auction/use-claim-tokens.ts
@@ -1,11 +1,30 @@
 import { useMutation } from 'react-query'
 import useDutchAuction from './use-dutch-auction'
+import { BigNumber } from 'ethers'
 
 const useClaimTokens = () => {
   const dutchAuction = useDutchAuction()
 
-  const request = async (amount: number) => dutchAuction?.claimTokens?.(amount)
+  const request = async (amount: number) => {
+    // Define gasLimit initially
+    let gasLimit = BigNumber.from(500000)
 
+    if (dutchAuction) {
+      try {
+        // Estimate the gas usage for your transaction
+        const estimate = await dutchAuction.estimateGas.claimTokens(amount)
+
+        // Set the gasLimit as estimatedGas * 1.2 (for 20% extra buffer)
+        gasLimit = estimate.mul(130).div(100)
+      } catch (err) {
+        console.log('Failed to estimate gas: ', err)
+      }
+    }
+
+    return dutchAuction?.claimTokens?.(amount, {
+      gasLimit: gasLimit,
+    })
+  }
   return useMutation(request)
 }
 
